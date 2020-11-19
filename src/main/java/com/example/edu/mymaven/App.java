b6b0052b486c1abbfd34956682b4e7cea84dc3a1
Morgan Farnworth <morganfarnworth6@gmail.com>
2020-11-19 23:21:03 +0100

    Vokabeltrainer ist jetzt ein Maven Project

--- src/main/java/com/example/edu/mymaven/App.java
+++ src/main/java/com/example/edu/mymaven/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" ); //Notitz
-    }
+        System.out.println( "Hello World!" ); //Notiz 2
+        }
 }
