cedb8aeddd7355f33a444660f2c5ef38f8cef443
Tanja Mohler <tanja.mohler@protonmail.ch>
2020-11-20 21:25:08 +0100

    Erstes Programmfenster

---
+++ src/main/java/com/vokabeltrainer/UserInterface.java
@@ -0,0 +1,30 @@
+package com.vokabeltrainer;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class UserInterface extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		try {
+			
+			GridPane gridPane = new GridPane();
+			
+			Scene scene = new Scene(gridPane);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Vokabeltrainer");
+			primaryStage.show();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/main/java/com/vokabeltrainer/Vokabeln.java
@@ -0,0 +1,5 @@
+package com.vokabeltrainer;
+
+public class Vokabeln {
+
+}
